3f1200889fc37c5aa5b9a2b5238139ad68021bc1
Yassine Bolard <yassine.bolard@etu.univ-tours.fr>
2025-05-12 15:25:28 +0000

    Ajout Exo 1

---
+++ 2025_BDD_BOLARD_YASSINE_EX1/README.md
@@ -0,0 +1,12 @@
+# Exercice 1
+
+Le Modèle Conceptuel de Données :
+![stacks](./img/mcd.png)
+
+Le Modèle Logique de Données :
+![stacks](./img/mld.png)
+
+Nous avons donc 3 entités distinctes:
+- Utilisateur
+- Materiel
+- Reservation
---
+++ 2025_BDD_BOLARD_YASSINE_EX1/script.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Utilisateur(
+   userid SERIAL,
+   nom VARCHAR(50)  NOT NULL,
+   prenom VARCHAR(50)  NOT NULL,
+   email VARCHAR(60)  NOT NULL,
+   PRIMARY KEY(userid)
+);
+
+CREATE TABLE Materiel(
+   materialid SERIAL,
+   nom VARCHAR(50)  NOT NULL,
+   PRIMARY KEY(materialid)
+);
+
+CREATE TABLE Reservation(
+   reservationid SERIAL,
+   date_debut TIMESTAMP NOT NULL,
+   date_fin TIMESTAMP NOT NULL,
+   materialid INTEGER NOT NULL,
+   userid INTEGER NOT NULL,
+   PRIMARY KEY(reservationid),
+   FOREIGN KEY(materialid) REFERENCES Materiel(materialid),
+   FOREIGN KEY(userid) REFERENCES Utilisateur(userid)
+);
